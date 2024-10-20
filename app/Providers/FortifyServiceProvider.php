4d0244b51137e914de538616cdfc70472461bcd8
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2024-10-20 08:23:16 +0300

    Addition of code to login based on user type

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -8,9 +8,11 @@ use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Laravel\Fortify\Contracts\LoginResponse;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -20,7 +22,19 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // // Customize the post-login redirect based on user role
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request)
+            {
+                $role = auth()->user()->user_type;
+
+                if ($role == 0) {
+                    return redirect()->intended(config('fortify.admin_home'));
+                }
+
+                return redirect()->intended(config('fortify.home'));
+            }
+        });
     }
 
     /**
