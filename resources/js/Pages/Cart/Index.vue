73976558e3ef2629df7e408ee72af02275414c47
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2024-10-23 12:48:27 +0300

    seperation of the ui

--- resources/js/Pages/Cart/Index.vue
+++ resources/js/Pages/Cart/Index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type {Order} from "@/Types/types";
+import ClientLayout from "@/Layouts/ClientLayout.vue";
 
 defineProps({
     order: {
@@ -10,7 +11,10 @@ defineProps({
 </script>
 
 <template>
+    <ClientLayout>
+
     <a href="">{{order}}</a>
+    </ClientLayout>
 </template>
 
 <style scoped>
